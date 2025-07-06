b2511c3eefe539401fe618536c77ce63bcb6e051
yuripnp <yuripnp1992@gmail.com>
2025-07-06 17:07:40 -0300

    finalizando

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, imagem
+from .views import index, imagem, buscar
 
 urlpatterns = [
     path('', index, name='index'),
     path('imagem/<str:foto_id>', imagem, name='imagem'),
+    path('buscar', buscar, name='buscar'),
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -8,3 +8,11 @@ def index(request):
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
+
+def buscar(request):
+    fotografias = Fotografia.objects.filter(publicada=True).order_by('-id')
+    if "buscar" in request.GET:
+        termo = request.GET['buscar']
+        if termo:
+            fotografias = fotografias.filter(nome__icontains=termo)
+    return render(request, 'galeria/buscar.html', {'fotografias': fotografias})
